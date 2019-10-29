832c939ac245e725a476538e6db8e5dce6904aa3
natthawad <natthawad.jorm@gmail.com>
2019-10-29 15:28:59 +0700

    resetpassword

--- src/app/reset-password/reset-password.module.ts
+++ src/app/reset-password/reset-password.module.ts
@@ -1,27 +1,27 @@
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
-// import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
-// import { ResetPasswordPage } from './reset-password.page';
+import { ResetPasswordPage } from './reset-password.page';
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: ResetPasswordPage
-//   }
-// ];
+const routes: Routes = [
+  {
+    path: '',
+    component: ResetPasswordPage
+  }
+];
 
-// @NgModule({
-//   imports: [
-//     CommonModule,
-//     FormsModule,
-//     ReactiveFormsModule,
-//     IonicModule,
-//     RouterModule.forChild(routes)
-//   ],
-//   declarations: [ResetPasswordPage]
-// })
-// export class ResetPasswordPageModule {}
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ResetPasswordPage]
+})
+export class ResetPasswordPageModule {}

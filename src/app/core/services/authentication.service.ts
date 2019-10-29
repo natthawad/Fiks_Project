636a51d94f93294ead2e922ede8cb3ccbee55210
natthawad <natthawad.jorm@gmail.com>
2019-10-29 11:38:25 +0700

    service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,74 +1,74 @@
-// import { Injectable } from '@angular/core';
-// import { RegisterInterface } from '../models/register.interface';
-// import { AuthInterface } from '../models/auth.interface';
-// import * as firebase from 'firebase/app';
-// import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-// import { Observable, of } from 'rxjs';
-// import { UserInterface } from '../models/user.interface';
-// import { AngularFireAuth } from '@angular/fire/auth';
-// import { switchMap, first} from 'rxjs/operators';
-// import { FireStoreDoc } from '../../_constants/app.constant';
+import { Injectable } from '@angular/core';
+import { RegisterInterface } from '../models/register.interface';
+import { AuthInterface } from '../models/auth.interface';
+import * as firebase from 'firebase/app';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { UserInterface } from '../models/user.interface';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, first} from 'rxjs/operators';
+import { FireStoreDoc } from '../../_constants/app.constant';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthenticationService {
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
 
-//   user$: Observable<UserInterface>;
+  user$: Observable<UserInterface>;
 
-//   constructor(
-//     private afAuth: AngularFireAuth,
-//     private afs: AngularFirestore
-//   ) {
-//     this.user$ = this.afAuth.authState.pipe(
-//       switchMap(user => {
-//         if (user) {
-//           return this.afs.doc<UserInterface>(${FireStoreDoc.USER_PROFILE}/${user.uid}).valueChanges();
-//         } else {
-//           return of(null);
-//         }
-//       })
-//     );
-//   }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<UserInterface>(${FireStoreDoc.USER_PROFILE}/${user.uid}).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
 
-//   getUser() {
-//     return this.user$.pipe(first()).toPromise();
-//   }
+  getUser() {
+    return this.user$.pipe(first()).toPromise();
+  }
 
-//   register(value: RegisterInterface): Promise<any> {
-//     return firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
-//       .then(
-//         (res) => this.updateProfile(res.user.uid, value),
-//         (err) => {}
-//       );
-//   }
+  register(value: RegisterInterface): Promise<any> {
+    return firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+      .then(
+        (res) => this.updateProfile(res.user.uid, value),
+        (err) => {}
+      );
+  }
 
-//   async loginUser(value: AuthInterface) {
-//     return await firebase.auth().signInWithEmailAndPassword(value.email, value.password);
-//   }
+  async loginUser(value: AuthInterface) {
+    return await firebase.auth().signInWithEmailAndPassword(value.email, value.password);
+  }
 
-//   async logoutUser() {
-//     return await firebase.auth().signOut();
-//   }
+  async logoutUser() {
+    return await firebase.auth().signOut();
+  }
 
-//   resetPassword(email: string): Promise<void> {
-//     return firebase.auth().sendPasswordResetEmail(email);
-//   }
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
 
-//   private updateProfile(uid: string, user: RegisterInterface) {
-//     const userRef: AngularFirestoreDocument<any> = this.afs.doc(${FireStoreDoc.USER_PROFILE}/${uid});
+  private updateProfile(uid: string, user: RegisterInterface) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(${FireStoreDoc.USER_PROFILE}/${uid});
 
-//     const data = {
-//       uid,
-//       email: user.email,
-//       fullName: user.fullName,
-//       address: user.address,
-//       phone: user.phone,
-//       imageProfile: user.imageProfile
-//       // imageProfile: user.imageProfile
-//     };
+    const data = {
+      uid,
+      email: user.email,
+      fullName: user.fullName,
+      address: user.address,
+      phone: user.phone,
+      imageProfile: user.imageProfile
+      // imageProfile: user.imageProfile
+    };
 
-//     userRef.set(data, { merge: true });
-//   }
+    userRef.set(data, { merge: true });
+  }
 
-// }
+}
--- src/app/core/services/cusrequst.service.ts
+++ src/app/core/services/cusrequst.service.ts
@@ -1,21 +1,21 @@
-// import { Injectable } from '@angular/core';
-// import * as firebase from 'firebase/app'
-// import { RegisterInterface } from '../_models/register.interface';
-// import { AuthInterface } from '../_models/auth.interface';
-// import { AngularFirestore, DocumentReference, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app'
+import { RegisterInterface } from '../_models/register.interface';
+import { AuthInterface } from '../_models/auth.interface';
+import { AngularFirestore, DocumentReference, AngularFirestoreCollection } from '@angular/fire/firestore';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class CusrequstsService {
-//   private userCusrequsts: AngularFirestoreCollection<any>;
-//   constructor(
-//     private afs: AngularFirestore
-//   ) { 
-//     this.userCusrequsts = this.afs.collection<any>('cusrequsts');
-//   }
+@Injectable({
+  providedIn: 'root'
+})
+export class CusrequstsService {
+  private userCusrequsts: AngularFirestoreCollection<any>;
+  constructor(
+    private afs: AngularFirestore
+  ) { 
+    this.userCusrequsts = this.afs.collection<any>('cusrequsts');
+  }
 
-// updateCusrequsts(data: any):Promise<DocumentReference>{
-//   return this.userCusrequsts.add(data);
-// }
-// }
+updateCusrequsts(data: any):Promise<DocumentReference>{
+  return this.userCusrequsts.add(data);
+}
+}
